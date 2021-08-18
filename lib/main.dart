920d75d12a54710a3b2e8383c95bef7e77be731b
mekides4325 <se.mekides.bishaw@gmail.com>
2021-08-18 14:05:15 +0300

    Creating the folder structure for the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:tourist_guide_app/screens/login_screen.dart';
 
-// void main() {
-//   runApp(MaterialApp(
-//     home: Login(),
-//   ));
-// }
-void main() => runApp(Login());
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Travel App UI',
+      debugShowCheckedModeBanner: false,
+      // home: WelcomeScreen(),
+    );
+  }
+}
