7725a751c9634ab4299b82e28930ae212fb2fa5c
Oaksas <aman.getnet2@gmail.com>
2021-08-20 12:13:00 -1100

    Creating basic DDD folder structure

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:tourist_guide_app/screens/login_screen.dart';
-
-// void main() {
-//   runApp(MaterialApp(
-//     home: Login(),
-//   ));
-// }
-void main() => runApp(Login());
