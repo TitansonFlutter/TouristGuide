2ae0eb516c6b973b0a58c3602f4c7406ef7c5071
Oaksas <se.woldeamanual.getnet@gmail.com>
2021-08-23 18:49:52 -0400

    creating filter page

---
+++ lib/Presentation/core/searchAndFilter/home.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Body(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
