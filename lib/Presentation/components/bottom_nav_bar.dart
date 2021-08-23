ba1d13dba3c1bdcdb8b8129ca23ff2daaf4f6fdf
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2021-08-23 20:59:00 +0300

    Bottom Navigation for the app is updated.

--- lib/Presentation/components/bottom_nav_bar.dart
+++ lib/Presentation/components/bottom_nav_bar.dart
@@ -1,82 +1,107 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+// import 'package:flutter_svg/svg.dart';
 //import 'package:flutter_svg/flutter_svg.dart';
 //import 'package:travel_app/constants.dart';
 
-class BottomNavBar extends StatelessWidget {
-  // const BottomNavBar({
-  //   Key key,
-  // }) : super(key: key);
+// class BottomNavBar extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 75,
+//       width: double.infinity,
+//       margin: EdgeInsets.all(15),
+//       padding: EdgeInsets.symmetric(horizontal: 40),
+//       decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(50),
+//           color: Colors.blue,
+//           boxShadow: [
+//             BoxShadow(
+//                 blurRadius: 20,
+//                 offset: const Offset(0, 3),
+//                 color: Colors.black.withAlpha(20))
+//           ]),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//         children: [
+//           NavItem(
+//             icon: 'assets/icons/home.svg',
+//             isActive: true,
+//           ),
+//           NavItem(
+//             icon: 'assets/icons/calendar.svg',
+//             isActive: false,
+//           ),
+//           NavItem(
+//             icon: 'assets/icons/search.svg',
+//             isActive: false,
+//           ),
+//           NavItem(
+//             icon: 'assets/icons/user.svg',
+//             isActive: false,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
+// class NavItem extends StatelessWidget {
+//   const NavItem({Key key, @required this.icon, @required this.isActive})
+//       : super(key: key);
+
+//   final String icon;
+//   final bool isActive;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Spacer(),
+//         IconButton(icon: SvgPicture.asset(icon), onPressed: () {}),
+//         Spacer(),
+//         Container(
+//           height: 4,
+//           width: 35,
+//           decoration: BoxDecoration(
+//               borderRadius: BorderRadius.circular(10),
+//               color: isActive ? Color(0XFF6A62B7) : Colors.transparent,
+//               boxShadow: [
+//                 BoxShadow(
+//                     blurRadius: 6,
+//                     offset: const Offset(0, -2),
+//                     color: isActive ? Color(0XFF6A62B7) : Colors.transparent)
+//               ]),
+//         )
+//       ],
+//     );
+//   }
+
+// }
+
+class BottomNavBar extends StatefulWidget {
+  List<String> _options = ["Home", "Search, Agents", "History"];
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 75,
-      width: double.infinity,
-      margin: EdgeInsets.all(15),
-      padding: EdgeInsets.symmetric(horizontal: 40),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(50),
-          color: Colors.blue,
-          boxShadow: [
-            BoxShadow(
-                blurRadius: 20,
-                offset: const Offset(0, 3),
-                color: Colors.black.withAlpha(20))
-          ]),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          NavItem(
-            icon: 'assets/icons/home.svg',
-            isActive: true,
-          ),
-          NavItem(
-            icon: 'assets/icons/calendar.svg',
-            isActive: false,
-          ),
-          NavItem(
-            icon: 'assets/icons/search.svg',
-            isActive: false,
-          ),
-          NavItem(
-            icon: 'assets/icons/user.svg',
-            isActive: false,
-          ),
-        ],
-      ),
-    );
-  }
+  _BottomNavBarState createState() => _BottomNavBarState();
 }
 
-class NavItem extends StatelessWidget {
-  const NavItem({Key key, @required this.icon, @required this.isActive})
-      : super(key: key);
-
-  final String icon;
-  final bool isActive;
-
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Spacer(),
-        IconButton(icon: SvgPicture.asset(icon), onPressed: () {}),
-        Spacer(),
-        Container(
-          height: 4,
-          width: 35,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: isActive ? Color(0XFF6A62B7) : Colors.transparent,
-              boxShadow: [
-                BoxShadow(
-                    blurRadius: 6,
-                    offset: const Offset(0, -2),
-                    color: isActive ? Color(0XFF6A62B7) : Colors.transparent)
-              ]),
-        )
+    return CurvedNavigationBar(
+      height: 60,
+      color: Color(0XFF6A62B7),
+      buttonBackgroundColor: Color(0XFF6A62B7),
+      backgroundColor: Colors.transparent,
+      // animationDuration: Duration(seconds: 1),
+      // animationCurve: Curves.bounceOut,
+      items: <Widget>[
+        Icon(Icons.home, color: Colors.white),
+        Icon(Icons.search, color: Colors.white),
+        Icon(Icons.favorite, color: Colors.white),
+        Icon(Icons.history, color: Colors.white),
       ],
     );
   }
