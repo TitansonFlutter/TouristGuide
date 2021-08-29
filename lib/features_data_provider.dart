7f0880d8fcd783992f019dbc163d759bc0845d1e
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2021-08-29 14:52:31 +0300

    The repository for the features api is done.

---
+++ lib/features_data_provider.dart
@@ -0,0 +1,47 @@
+// import 'dart:convert';
+
+// import 'package:http/http.dart' as http;
+
+// class FeaturesDataProvider {
+//   static final String _url = "http://localhost/api/features";
+
+// // Get all Tours
+//   Future<Tour> fetchAllTours() async {
+//     final response = await http.get(Uri.parse("$_url/tours"));
+
+//     if (response.statusCode == 200) {
+//       final tours = jsonDecode(response.body) as List;
+//       return tours.map((tour) => Tour.fromJson(tour)).toList();
+//     } else {
+//       throw Exception("Could not fetch tours");
+//     }
+//   }
+
+// // Add a Reviews
+//   Future<Review> addReview(Review review) async {
+//     final http.Response response = await http.post(Uri.parse("$_url/review"),
+//         headers: <String, String>{"Content-Type": "application/json"},
+//         body: jsonEncode({
+//           "BookId": review.bookId,
+//           "UserId": review.userId,
+//           "Comment": review.comment,
+//           "Rate": review.rate,
+//           "Date": review.date
+//         }));
+//     if (response.statusCode == 201) {
+//       return Review.fromJson(jsonDecode(response.body));
+//     } else {
+//       throw Exception("Failed to add your Review");
+//     }
+//   }
+
+//   Future<Tour> getTour(int id, Tour tour) async {
+//     final response = await http.get(Uri.parse("$_url/tours/$id"));
+
+//     if (response.statusCode == 200) {
+//       return Tour.fromJson(jsonDecode(response.body));
+//     } else {
+//       throw Exception("Could not get the tour");
+//     }
+//   }
+// }
---
+++ lib/repository/features_repository.dart
@@ -0,0 +1,19 @@
+// import 'package:tourist_guide_app/features_data_provider.dart';
+
+// class FeaturesRepository {
+//   final FeaturesDataProvider _dataProvider;
+
+//   FeaturesRepository(this._dataProvider);
+
+//   Future<Tour> fetchAllTours() async {
+//     return this._dataProvider.fetchAllTours();
+//   }
+
+//   Future<Review> addReview(Review review) async {
+//     return this._dataProvider.addReview(review);
+//   }
+
+//   Future<Tour> addReview(int id, Tour review) async {
+//     return this._dataProvider.getTour(id, tour);
+//   }
+// }
