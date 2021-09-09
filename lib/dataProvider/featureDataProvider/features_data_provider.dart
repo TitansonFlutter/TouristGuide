6ac9d5c6ec37e15574c8233515c77d55ebb071a2
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2021-09-09 22:36:47 +0300

    Data provider for features updated.

--- lib/dataProvider/featureDataProvider/features_data_provider.dart
+++ lib/dataProvider/featureDataProvider/features_data_provider.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:tourist_guide_app/AuthException.dart';
+import 'package:tourist_guide_app/Presentation/Models/User.dart';
+import 'package:tourist_guide_app/Presentation/Models/review_model.dart';
 
 import 'package:tourist_guide_app/Presentation/Models/tour_list.dart';
 
 class FeaturesDataProvider {
-  static final String _url = "http://localhost/api/features";
+  static final String _url = "http://10.6.250.16:5000/api";
 
 // Get all Tours
   Future<List<Tour>> fetchAllTours() async {
-    final response = await http.get(Uri.parse("$_url/tours"));
-
+    final response = await http.get(Uri.parse("$_url/agents/2/tours"));
     if (response.statusCode == 200) {
       final tours = jsonDecode(response.body) as List;
       return tours.map((tour) => Tour.fromJson(tour)).toList();
@@ -21,23 +22,39 @@ class FeaturesDataProvider {
   }
 
 // // Add a Reviews
-//   Future<Review> addReview(Review review) async {
-//     final http.Response response = await http.post(Uri.parse("$_url/review"),
-//         headers: <String, String>{"Content-Type": "application/json"},
-//         body: jsonEncode({
-//           "BookId": review.bookId,
-//           "UserId": review.userId,
-//           "Comment": review.comment,
-//           "Rate": review.rate,
-//           "Date": review.date
-//         }));
-//     if (response.statusCode == 201) {
-//       return Review.fromJson(jsonDecode(response.body));
-//     } else {
-//       throw Exception("Failed to add your Review");
-//     }
-//   }
+  Future<Review> addReview(Review review) async {
+    final http.Response response =
+        await http.post(Uri.parse("$_url/features/reviews"),
+            headers: <String, String>{"Content-Type": "application/json"},
+            body: jsonEncode({
+              "UserId": review.userId,
+              "Name": review.tourId,
+              "Comment": review.comment,
+              "Rate": review.rate,
+              "Date": review.date
+            }));
+    if (response.statusCode == 201) {
+      return Review.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to add your Review");
+    }
+  }
 
+  // GEt All Reviews
+  Future<List<Review>> fetchAllReviews(int tId) async {
+    final response = await http.get(Uri.parse("$_url/features/reviews/$tId"));
+    if (response.statusCode == 200) {
+      final tours = jsonDecode(response.body) as List;
+      print(tours);
+      return tours.map((tour) => Review.fromJson(tour)).toList();
+    } else {
+      // print("Error");
+      final err = jsonDecode(response.body);
+      throw AuthException(err["message"]);
+    }
+  }
+
+// Get Tour by Id
   Future<Tour> getTour(int id, Tour tour) async {
     final response = await http.get(Uri.parse("$_url/tours/$id"));
 
@@ -48,37 +65,13 @@ class FeaturesDataProvider {
     }
   }
 
-//Filter
-  // Future<Tour> filter(List<String> filters) async {
-  //   final http.Response response = await http.post(Uri.parse("$_url/tours"),
-  //       headers: <String, String>{"Content-Type": "application/json"},
-  //       body: jsonEncode({
-  //         "PriceHigh": filters.highPrice,
-  //         "PriceLow": filters.lowPrice,
-  //         "Speciality": filters.special
-  //       }));
-  //   if (response.statusCode == 200) {
-  //     return Review.fromJson(jsonDecode(response.body));
-  //   } else {
-  //     throw Exception("Something went wrong");
-  //   }
-  // }
+  Future<User> bookStatus(int uId, int tId) async {
+    final response = await http.get(Uri.parse("$_url/users/$uId/book/$tId"));
 
-// Future<Tour> getAllTours() async {
-//     final http.Response response = await http.get(Uri.parse("$_url/tours"));
-//     if (response.statusCode == 200) {
-//       return Review.fromJson(jsonDecode(response.body));
-//     } else {
-//       throw Exception("Something went wrong");
-//     }
-//   }
-
-// Future<Tour> getTourByName(String tourName) async {
-//     final http.Response response = await http.get(Uri.parse("$_url/tours/$tourName"));
-//     if (response.statusCode == 200) {
-//       return Review.fromJson(jsonDecode(response.body));
-//     } else {
-//       throw Exception("Something went wrong");
-//     }
-//   }
+    if (response.statusCode == 200) {
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Could not get Book Status");
+    }
+  }
 }
